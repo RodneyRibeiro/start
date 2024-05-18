af8993111858134b8fc507dbce8aa5c08624df2e
Rodney Ribeiro <136312023+RodneyRibeiro@users.noreply.github.com>
2024-05-18 12:39:01 +0100

    Update desafio Dio saque-deposito-extrato.py

--- desafio Dio saque-deposito-extrato.py
+++ desafio Dio saque-deposito-extrato.py
@@ -1,3 +1,14 @@
+'''
+DESAFIO:
+Paulinho tem em suas mãos um novo problema. Agora a sua professora lhe pediu que construísse um programa para verificar, à partir de dois valores muito grandes A e B, se B corresponde aos últimos dígitos de A.
+
+ENTRADA:
+A entrada consiste de vários casos de teste. A primeira linha de entrada contém um inteiro N que indica a quantidade de casos de teste. Cada caso de teste consiste de dois valores A e B maiores que zero, cada um deles podendo ter até 1000 dígitos.
+
+SAÍDA:
+Para cada caso de entrada imprima uma mensagem indicando se o segundo valor encaixa no primeiro valor, confome exemplo abaixo.
+'''
+
 #variáveis
 saldo = 1000.0
 saques_total=3
